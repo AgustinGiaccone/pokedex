a5668f5e94757530569711c906a0ffde2f31877a
Agustin Giaccone <agustintgiaccone1999@gmail.com>
2023-09-15 17:57:45 -0300

    mostrando los primeros 20 pokemones(nombre,img,ID)

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [pokemones, setPokemones]= useState([]) //estado donde se guardan los pokemones
+
+  useEffect(()=>{
+
+    const getPokemones = async () =>{
+      // recuperamos el estado de los pokemones
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+      const ListaPokemones = await response.json()
+      const { results } = ListaPokemones
+
+      const newPokemones =results.map(async(pokemon) =>{
+        const response = await fetch(pokemon.url)
+        const poke = await response.json()
+
+        return {
+          id: poke.id,
+          name: poke.name,
+          img: poke.sprites.other.dream_world.front_default
+        }
+      })
+
+      setPokemones(await Promise.all(newPokemones))
+    }
+    getPokemones()
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>POKEDEX</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <h1>Pokedex</h1>
+
+      {
+        pokemones.map(pokemon => {
+          return(
+            <div>
+              <img src={pokemon.img} alt={pokemon.name}></img>
+              <p>{pokemon.name}</p>
+              <span>{pokemon.id}</span>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
