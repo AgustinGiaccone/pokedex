356b2383c179d03ea811a34a78067f59255c8b35
Agustin Giaccone <agustintgiaccone1999@gmail.com>
2023-10-27 18:25:32 -0300

    repsaso y traemos las img 3D

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,4 +1,4 @@
-
+// PARA CORRER EL PRO
 import { useEffect, useState } from 'react'
 import './App.css'
 
@@ -9,10 +9,13 @@ function App() {
   useEffect(()=>{
 
     const getPokemones = async () =>{
+
       // recuperamos el estado de los pokemones
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=700&offset=0') //trae los pokemones que necesitamos en este caso son 700 pokemones  //como es una pormesa se necesita la palabra reservada await
+
       const ListaPokemones = await response.json()
-      const { results } = ListaPokemones
+      const { results } = ListaPokemones   //en result se encuentr atodos los datos que necesitamos de los pokemones
 
       const newPokemones =results.map(async(pokemon) =>{
         const response = await fetch(pokemon.url)
@@ -21,8 +24,11 @@ function App() {
         return {
           id: poke.id,
           name: poke.name,
-          img: poke.sprites.other.dream_world.front_default
+          img: poke.sprites.other.dream_world.front_default,  //imagen 2d
+          img1:poke.sprites.other.home.front_default,         //imagen 3d #1
+          img2:poke.sprites.other.home.front_shiny            ////imagen 3d #2
         }
+        
       })
 
       setPokemones(await Promise.all(newPokemones))
@@ -39,8 +45,10 @@ function App() {
           return(
             <div>
               <img src={pokemon.img} alt={pokemon.name}></img>
+              {/* <img src={pokemon.img1} alt={pokemon.name}></img>             las imagenes 3d comentadas por ser demaciadas pesadas
+              <img src={pokemon.img2} alt={pokemon.name}></img> */}
               <p>{pokemon.name}</p>
-              <span>{pokemon.id}</span>
+              <span>#{pokemon.id}</span>
             </div>
           )
         })
